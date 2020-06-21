358310d13600f59bf52bdaf390b58bf2e076cd50
Nier <com.owl.minerva@gmail.com>
2020-06-21 11:28:32 +0700

    x

--- app/src/main/java/com/miracledmi/controller/DogeController.kt
+++ app/src/main/java/com/miracledmi/controller/DogeController.kt
@@ -44,8 +44,11 @@ class DogeController(private var body: HashMap<String, String>) : AsyncTask<Void
             convertJSON.toString().contains("MaxPayoutExceeded") -> {
               JSONObject().put("code", 404).put("data", "Max Payout Exceeded")
             }
+            convertJSON.toString().contains("www.999doge.com") -> {
+              JSONObject().put("code", 404).put("data", "Invalid request")
+            }
             convertJSON.toString().contains("error") -> {
-              JSONObject().put("code", 500).put("data", "Invalid request")
+              JSONObject().put("code", 404).put("data", "Invalid request")
             }
             else -> {
               JSONObject().put("code", 200).put("data", convertJSON)
